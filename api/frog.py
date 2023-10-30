cc255119a1bc3c99d4cb3df4808ce11ea639ebd3
sergi1207 <sergi1207@gmail.com>
2023-10-30 15:00:02 -0700

    Updated frog

--- api/frog.py
+++ api/frog.py
@@ -1,56 +1,18 @@
 from flask import Blueprint, jsonify
 from flask_restful import Api, Resource
 import random
-from model.frog_items import *
+import requests  # Add this import statement
+from model.frog_items import *  # Make sure this import is correct
 
 frog_item_api = Blueprint('frog_item_api', __name__,
                    url_prefix='/api/frog-items')
 
 api = Api(frog_item_api)
 
-class FrogItemsAPI:
-    class _Create(Resource):
-        def post(self, item):
-            pass
-            
-    class _Read(Resource):
-        def get(self):
-            return jsonify(getFrogItems())
-
-    class _ReadID(Resource):
-        def get(self, id):
-            return jsonify(getFrogItem(id))
-
-    class _ReadRandom(Resource):
-        def get(self):
-            return jsonify(getRandomFrogItem())
-    
-    class _ReadCount(Resource):
-        def get(self):
-            count = countFrogItems()
-            countMsg = {'count': count}
-            return jsonify(countMsg)
-
-    class _UpdateLike(Resource):
-        def put(self, id):
-            addFrogItemLike(id)
-            return jsonify(getFrogItem(id))
-
-    class _UpdateDislike(Resource):
-        def put(self, id):
-            addFrogItemDislike(id)
-            return jsonify(getFrogItem(id))
-
-    api.add_resource(_Create, '/create/<string:item>')
-    api.add_resource(_Read, '/')
-    api.add_resource(_ReadID, '/<int:id>')
-    api.add_resource(_ReadRandom, '/random')
-    api.add_resource(_ReadCount, '/count')
-    api.add_resource(_UpdateLike, '/like/<int:id>')
-    api.add_resource(_UpdateDislike, '/dislike/<int:id>')
+# Rest of your code remains the same
 
 if __name__ == "__main__": 
-    server = 'https://flask.nighthawkcodingsociety.com'
+    server = 'https://your-server-url-here'  # Replace with your actual server URL
     url = server + "/api/frog-items"
     responses = []
 
