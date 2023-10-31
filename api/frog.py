47bc4155b568ea7a1534afd4628a978263c30bb5
sergi1207 <sergi1207@gmail.com>
2023-10-30 20:41:25 -0700

    test

--- api/frog.py
+++ api/frog.py
@@ -11,33 +11,36 @@ api = Api(frog_item_api)
 
 # Rest of your code remains the same
 
-if __name__ == "https://projectsailbackend.stu.nighthawkcodingsociety.com": 
+if __name__ == '__main__':
     server = 'https://projectsailbackend.stu.nighthawkcodingsociety.com'  # Replace with your actual server URL
     url = server + "/api/frog-items"
     responses = []
 
-    count_response = requests.get(url+"/count")
+    count_response = requests.get(url + "/count")
     count_json = count_response.json()
     count = count_json['count']
 
-    num = str(random.randint(0, count-1))
+    num = str(random.randint(0, count - 1))
     responses.append(
-        requests.get(url+"/"+num)
-    ) 
+        requests.get(url + "/" + num)
+    )
     responses.append(
-        requests.put(url+"/like/"+num)
-    ) 
+        requests.put(url + "/like/" + num)
+    )
     responses.append(
-        requests.put(url+"/dislike/"+num)
-    ) 
+        requests.put(url + "/dislike/" + num)
+    )
 
     responses.append(
-        requests.get(url+"/random")
-    ) 
+        requests.get(url + "/random")
+    )
 
     for response in responses:
         print(response)
         try:
-            print(response.json())
-        except:
-            print("unknown error")
+            response_json = response.json()
+            print(response_json)
+        except ValueError:
+            print("Error: Invalid JSON response")
+        except Exception as e:
+            print(f"An error occurred: {str(e)}")
